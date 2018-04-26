aa53dd5e8d02209f86070b87fe9af9d2aeba245e
Eugene <eugene.github@gmail.com>
2018-04-26 13:21:54 +0300

    Missed file

---
+++ Altium/BigSorter/SelectComparer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Altium.BigSorter
+{
+  public class SelectComparer<K, T> : IComparer<K>
+  {
+    private readonly Func<K, T> _select;
+    private readonly IComparer<T> _comparer;
+
+    public SelectComparer(Func<K, T> select, IComparer<T> comparer)
+    {
+      _select = select;
+      _comparer = comparer;
+    }
+
+    public int Compare(K x, K y)
+    {
+      return _comparer.Compare(_select(x), _select(y));
+    }
+  }
+}
