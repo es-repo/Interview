cc473cf58e3b2937ac87f35859df427acf5bc6f7
Eugene Sirota <ES-Macbook@es-mbr.local>
2018-05-02 03:12:18 +0300

    Unit tests

--- Altium/BigSorter.Tests/RecordsBufferTests.cs
+++ Altium/BigSorter.Tests/RecordsBufferTests.cs
@@ -1,99 +1,99 @@
-// using System.Collections.Generic;
-// using System.IO;
-// using Altium.BigSorter;
-// using Xunit;
+using System.Collections.Generic;
+using System.IO;
+using Altium.BigSorter;
+using Xunit;
 
-// namespace Altium.BigSorter.Tests
-// {
-//   public class RecordsBufferTests
-//   {
-//     [Fact]
-//     public void TestAddRecord()
-//     {
-//       object[][] records = new object[][]
-//       {
-//         new object[] { 0x1, "a" },
-//         new object[] { 0x2, "bc" },
-//         new object[] { 0x4, "def" },
-//         new object[] { 0x8, "xyz" }
-//       };
+namespace Altium.BigSorter.Tests
+{
+  public class RecordsBufferTests
+  {
+    [Fact]
+    public void TestAddRecord()
+    {
+      Record[] records = new Record[]
+      {
+        new Record ( 0x1, "a" ),
+        new Record ( 0x2, "bc" ),
+        new Record ( 0x4, "def" ),
+        new Record ( 0x8, "xyz" )
+      };
 
-//       long recordsBufferLen = 0;
-//       for (int i = 0; i < records.Length - 1; i++)
-//       {
-//         recordsBufferLen += (sizeof(int) + ((string) records[i][1]).Length * sizeof(char));
-//       }
+      long recordsBufferLen = 0;
+      for (int i = 0; i < records.Length - 1; i++)
+      {
+        recordsBufferLen += records[i].SizeInBytes;
+      }
 
-//       recordsBufferLen += 2;
+      recordsBufferLen += 2;
 
-//       RecordsBuffer recordsBuffer = new RecordsBuffer(recordsBufferLen);
+      RecordsBuffer recordsBuffer = new RecordsBuffer(recordsBufferLen);
 
-//       for (int i = 0; i < records.Length - 1; i++)
-//       {
-//         bool added = recordsBuffer.AddRecord(records[i]);
-//         Assert.True(added);
-//       }
+      for (int i = 0; i < records.Length - 1; i++)
+      {
+        bool added = recordsBuffer.AddRecord(records[i]);
+        Assert.True(added);
+      }
 
-//       bool addedLast = recordsBuffer.AddRecord(records[records.Length - 1]);
-//       Assert.False(addedLast);
-//       Assert.Equal(3, recordsBuffer.Records.Count);
-//     }
+      bool addedLast = recordsBuffer.AddRecord(records[records.Length - 1]);
+      Assert.False(addedLast);
+      Assert.Equal(3, recordsBuffer.Records.Count);
+    }
 
-//     [Fact]
-//     public void TestSort()
-//     {
-//       object[][] records = new object[][]
-//       {
-//         new object[] { 1, "a" },
-//         new object[] { 2, "aa" },
-//         new object[] { 7, "b" },
-//         new object[] { 2, "ab" },
-//         new object[] { 8, "aa" },
-//         new object[] { 4, "ba" },
-//         new object[] { 5, "bb" }
-//       };
+    [Fact]
+    public void TestSort()
+    {
+      Record[] records = new Record[]
+      {
+        new Record ( 1, "a"),
+        new Record ( 2, "aa" ),
+        new Record ( 7, "b" ),
+        new Record ( 2, "ab" ),
+        new Record ( 8, "aa" ),
+        new Record ( 4, "ba" ),
+        new Record ( 5, "bb" )
+      };
 
-//       RecordsBuffer recordsBuffer = new RecordsBuffer(200);
-//       for (int i = 0; i < records.Length; i++)
-//       {
-//         recordsBuffer.AddRecord(records[i]);
-//       }
+      RecordsBuffer recordsBuffer = new RecordsBuffer(200);
+      for (int i = 0; i < records.Length; i++)
+      {
+        recordsBuffer.AddRecord(records[i]);
+      }
 
-//       recordsBuffer.Sort(1);
+      recordsBuffer.Sort(1);
 
-//       object[][] expectedSortedByStringRecords = new object[][]
-//       {
-//         new object[] { 1, "a" },
-//         new object[] { 2, "aa" },
-//         new object[] { 8, "aa" },
-//         new object[] { 2, "ab" },
-//         new object[] { 7, "b" },
-//         new object[] { 4, "ba" },
-//         new object[] { 5, "bb" }
-//       };
+      Record[] expectedSortedByStringRecords = new Record[]
+      {
+        new Record ( 1, "a" ),
+        new Record ( 2, "aa" ),
+        new Record ( 8, "aa" ),
+        new Record ( 2, "ab" ),
+        new Record ( 7, "b" ),
+        new Record ( 4, "ba" ),
+        new Record ( 5, "bb" )
+      };
 
-//       for (int i = 0; i < recordsBuffer.Records.Count; i++)
-//       {
-//         Assert.Equal(expectedSortedByStringRecords[i], recordsBuffer.Records[i]);
-//       }
+      for (int i = 0; i < recordsBuffer.Records.Count; i++)
+      {
+        Assert.Equal(expectedSortedByStringRecords[i], recordsBuffer.Records[i]);
+      }
 
-//       recordsBuffer.Sort(0);
+      recordsBuffer.Sort(0);
 
-//       object[][] expectedSortedByIntRecords = new object[][]
-//       {
-//         new object[] { 1, "a" },
-//         new object[] { 2, "aa" },
-//         new object[] { 2, "ab" },
-//         new object[] { 4, "ba" },
-//         new object[] { 5, "bb" },
-//         new object[] { 7, "b" },
-//         new object[] { 8, "aa" }
-//       };
+      Record[] expectedSortedByIntRecords = new Record[]
+      {
+        new Record ( 1, "a" ),
+        new Record ( 2, "aa" ),
+        new Record ( 2, "ab" ),
+        new Record ( 4, "ba" ),
+        new Record ( 5, "bb" ),
+        new Record ( 7, "b" ),
+        new Record ( 8, "aa" )
+      };
 
-//       for (int i = 0; i < recordsBuffer.Records.Count; i++)
-//       {
-//         Assert.Equal(expectedSortedByIntRecords[i], recordsBuffer.Records[i]);
-//       }
-//     }
-//   }
-// }
+      for (int i = 0; i < recordsBuffer.Records.Count; i++)
+      {
+        Assert.Equal(expectedSortedByIntRecords[i], recordsBuffer.Records[i]);
+      }
+    }
+  }
+}
