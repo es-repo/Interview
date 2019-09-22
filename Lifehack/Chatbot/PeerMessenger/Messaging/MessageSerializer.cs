a5b89f4a576e4dc35307f11f0d2c457cdab282ba
Eugene Sirota <ES-Macbook@9.0.168.192.in-addr.arpa>
2019-09-22 10:25:33 +0300

    Remove commented code

--- Lifehack/Chatbot/PeerMessenger/Messaging/MessageSerializer.cs
+++ Lifehack/Chatbot/PeerMessenger/Messaging/MessageSerializer.cs
@@ -7,32 +7,6 @@ namespace PeerMessenger.Messaging
     public string DeserializeMessageBody(string input)
     {
       return JsonConvert.DeserializeAnonymousType(input, new { body = "" }).body;
-
-      // if (!input.StartsWith("(") && !input.EndsWith(")"))
-      // {
-      //   throw new ArgumentException("Input should start with '(' and and with ')'");
-      // }
-
-      // int startIndex = 0;
-      // int length = -1;
-      // for (int i = 0; i < input.Length; i++)
-      // {
-      //   if (input[i] == ')')
-      //   {
-      //     yield return input.Substring(startIndex + 1, length);
-      //     startIndex = i + 1;
-      //     length = -1;
-
-      //     if (startIndex < input.Length && input[startIndex] != '(')
-      //     {
-      //       throw new ArgumentException($"'(' is expected at {startIndex} position");
-      //     }
-      //   }
-      //   else
-      //   {
-      //     length++;
-      //   }
-      // }
     }
 
     public string SerializeMessageBody(string body)
